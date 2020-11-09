6031e10480dd4176ab03eb09b64b0e7064082603
fmouffok <fmouffok@protonmail.com>
2020-11-09 15:27:30 +0100

    add rooterLink for books category

--- bookstore/src/main/java/fr/km/bookstore/dao/BookRepository.java
+++ bookstore/src/main/java/fr/km/bookstore/dao/BookRepository.java
@@ -1,9 +1,17 @@
 package fr.km.bookstore.dao;
 
 import fr.km.bookstore.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+
 
 @CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    @RestResource(path = "categoryId")
+    Page<Book> findByCategoryId(@Param("id") Integer id, Pageable pageable);
 }
